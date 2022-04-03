ed6cb34e3615573e8915019a04d80f1b5d7f2809
Krzysztof Kuriata <krzysztof@kuriata.me>
2022-04-03 19:05:38 +0200

    ci: added github actions workflow for ci

---
+++ apps/clients/mn-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meta-nerf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mn-web';
+}
